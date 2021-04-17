05fcc9d45ca33ce0daa3787c88036dfe1f4d989b
Rafael Gerônimo <rafaelgeronimo@outlook.com>
2021-04-17 04:23:55 -0300

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import "./styles.css";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+function App() {
+  return <div>Hello, TrybeWallet!</div>;
 }
+
+export default App;
